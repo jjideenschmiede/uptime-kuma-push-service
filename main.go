68193dad3070c9923b6e345061960c7fb8b435b3
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2021-11-25 10:07:44 +0100

    Add main function

--- main.go
+++ main.go
@@ -9,4 +9,79 @@
 //
 //********************************************************************************************************************//
 
-package uptime_kuma_server_push
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/go-co-op/gocron"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+// UptimeKumaReturn is to decode the json return
+type UptimeKumaReturn struct {
+	Ok  bool   `json:"ok"`
+	Msg string `json:"msg,omitempty"`
+}
+
+func main() {
+
+	// Get environment & check the length
+	url := os.Getenv("URL")
+	msg := os.Getenv("MSG")
+	cron := os.Getenv("CRON")
+
+	if len(msg) <= 0 {
+		msg = "OK"
+	}
+
+	// Define cronjobs
+	s := gocron.NewScheduler(time.UTC)
+
+	s.Cron(cron).Do(func() {
+
+		// To get milliseconds
+		start := time.Now()
+
+		// Define client for request
+		client := &http.Client{}
+
+		// Define request & add url parameter
+		request, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		parameters := request.URL.Query()
+		parameters.Add("msg", msg)
+		parameters.Add("ping", fmt.Sprintf("%d", time.Since(start).Milliseconds()))
+		request.URL.RawQuery = parameters.Encode()
+
+		// Define response & send request
+		response, err := client.Do(request)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		// Decode json return
+		var decode UptimeKumaReturn
+
+		err = json.NewDecoder(response.Body).Decode(&decode)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		// Check decode & log to console
+		if decode.Ok {
+			log.Println("The heartbeat was successfully sent to your system.")
+		}
+
+	})
+
+	// Start scheduler
+	s.StartBlocking()
+
+}
